1469bb7dfd9022b634b3895a6758398c7d033dd1
danial <danialsiavashani@yahoo.com>
2024-08-13 20:51:52 -0700

    End of section 2

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@nextui-org/react';
+import Link from 'next/link';
+import { FaRegSmile } from 'react-icons/fa';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl ">Hello app!</h1>
+      <Button
+        as={Link}
+        href="/members"
+        color="primary"
+        variant="bordered"
+        startContent={<FaRegSmile size={20} />}
+        className="mt-4 items-center"
+      >
+        Click me
+      </Button>
+    </div>
+  );
+}
---
+++ src/components/navbar/TopNav.tsx
@@ -0,0 +1,60 @@
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from '@nextui-org/react';
+import Link from 'next/link';
+import { GiMatchTip } from 'react-icons/gi';
+import NavLink from './NavLink';
+
+export default function TopNav() {
+  return (
+    <div>
+      <Navbar
+        maxWidth="xl"
+        className="bg-gradient-to-r from-purple-400 to-purple-700"
+        classNames={{
+          item: [
+            'text-xl',
+            'text-whit',
+            'uppercase',
+            'data-[active=true]:text-yellow-200',
+          ],
+        }}
+      >
+        <NavbarBrand as={Link} href="/">
+          <GiMatchTip size={40} className="text-gray-200" />
+          <div className="font-bold text-3xl flex">
+            <span className="text-gray-900">Next</span>
+            <span className="text-gray-200">Match</span>
+          </div>
+        </NavbarBrand>
+        <NavbarContent justify="center">
+          <NavLink href="/members" label="matches" />
+          <NavLink href="/lists" label="Lists" />
+          <NavLink href="/messages" label="messages" />
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <Button
+            variant="bordered"
+            className="text-white"
+            as={Link}
+            href="/login"
+          >
+            Login
+          </Button>
+          <Button
+            variant="bordered"
+            className="text-white"
+            as={Link}
+            href="/register"
+          >
+            Register
+          </Button>
+        </NavbarContent>
+      </Navbar>
+    </div>
+  );
+}
